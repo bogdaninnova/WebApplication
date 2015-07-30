8078372afa8a566cf6264589b3d0792282ef0d16
bogdaninnova <bogdan.innova@gmail.com>
2015-07-30 13:30:16 +0300

    Create Transaction.java

---
+++ src/main/java/ua/sumdu/group73/model/objects/Transaction.java
@@ -0,0 +1,84 @@
+package ua.sumdu.group73.model.objects;
+
+import java.util.Date;
+
+public class Transaction {
+
+	private int id;
+	private int buyerID;
+	private int sellerID;
+	private int productID;
+	private int price;
+	private Date saleDate;
+	
+	
+	public Transaction(int id, int buyerID, int sellerID, int productID, int price, Date saleDate) {
+		this.setId(id);
+		this.setBuyerID(buyerID);
+		this.setSellerID(sellerID);
+		this.setProductID(productID);
+		this.setPrice(price);
+		this.setSaleDate(saleDate);
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getBuyerID() {
+		return buyerID;
+	}
+
+
+	public void setBuyerID(int buyerID) {
+		this.buyerID = buyerID;
+	}
+
+
+	public int getProductID() {
+		return productID;
+	}
+
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+
+	public int getSellerID() {
+		return sellerID;
+	}
+
+
+	public void setSellerID(int sellerID) {
+		this.sellerID = sellerID;
+	}
+
+
+	public Date getSaleDate() {
+		return saleDate;
+	}
+
+
+	public void setSaleDate(Date saleDate) {
+		this.saleDate = saleDate;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
