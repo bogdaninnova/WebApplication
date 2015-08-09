e61ad6c99d9d8a767e89adc4e6dd1b87d2a95677
bogdaninnova <bogdan.innova@gmail.com>
2015-08-09 13:50:59 +0300

    Update PicturesDBInterface.java

--- src/main/java/ua/sumdu/group73/model/interfaces/PicturesDBInterface.java
+++ src/main/java/ua/sumdu/group73/model/interfaces/PicturesDBInterface.java
@@ -2,10 +2,14 @@ package ua.sumdu.group73.model.interfaces;
 
 import java.util.List;
 
+import ua.sumdu.group73.model.objects.Picture;
+
 public interface PicturesDBInterface {
 
-	public List<String> getPicturesURLs(int productID);
+	public List<Picture> getPictures(int productID);
 	
 	public boolean addPictures(int productID, String URL);
+	
+	public List<Picture> getAllPictures();
 	
 }
