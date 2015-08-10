06005f22c8074c6d22a40d8864657f509e2d55e8
GREEN <gdvdima2008@yandex.ru>
2015-08-10 15:40:51 +0300

    Returns changes made by Bogdan.

--- src/main/java/ua/sumdu/group73/servlets/IndexServlet.java
+++ src/main/java/ua/sumdu/group73/servlets/IndexServlet.java
@@ -66,7 +66,7 @@ public class IndexServlet extends HttpServlet {
             sendResponse(response, "<result>OK</result>");
         } else if ("logOut".equals(request.getParameter("action"))) {
             if (session.getAttribute("user") != null || session.getAttribute("user") != "") {
-            	request.getSession().removeAttribute("user");
+            	session.setAttribute("user", "");
                 sendResponse(response, "<result>OK</result>");
             }
         } else if ("getProducts".equals(request.getParameter("action"))) {
