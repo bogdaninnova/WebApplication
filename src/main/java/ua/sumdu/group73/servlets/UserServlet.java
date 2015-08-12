f2350b7ceb02c6fe3d38fc6a19bf45fe73c879aa
GREEN <gdvdima2008@yandex.ru>
2015-08-12 13:51:33 +0300

    Created UserServlet.

---
+++ src/main/java/ua/sumdu/group73/servlets/UserServlet.java
@@ -0,0 +1,22 @@
+package ua.sumdu.group73.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * This servlet working with index.jsp.
+ *
+ * Created by Greenberg Dima <gdvdima2008@yandex.ru>
+ */
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
